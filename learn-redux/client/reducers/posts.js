a1b70b9cd0fb0e809fc238aca6993f7db1565fd1
Andy Lincoln <andrewlincoln11@gmail.com>
2016-11-13 15:21:59 -0500

    Chapter 7 - All about Redux Reducers

---
+++ learn-redux/client/reducers/posts.js
@@ -0,0 +1,11 @@
+// A reducer takes in two things:
+
+// 1. The Action (info about what happened)
+// 2. The copy of the curtrent state
+
+function posts(state = [], action) {
+  console.log(state, action);
+  return state;
+}
+
+export default posts;
